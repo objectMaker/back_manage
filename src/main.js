704c67bdf4ab3a697cdf5cbb09459d74d72fa436
objectMaker <528154320@qq.com>
2022-03-19 12:03:56 +0800

    '重置样式表'

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "@/assets/css/normalize.css"; //重置样式
 
 createApp(App).use(router).mount("#app");
