bcd61742a7174ab839d37196a6f7ce96d45a559b
objectMaker <528154320@qq.com>
2022-03-19 12:07:04 +0800

    '添加公共样式'

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "@/assets/css/normalize.css"; //重置样式
+import "@/assets/css/global.css"; //公共样式
 
 createApp(App).use(router).mount("#app");
